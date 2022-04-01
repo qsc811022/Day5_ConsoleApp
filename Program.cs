84cd152bcd8fb5969acdbd8c941ad6279e0c6d00
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-04-01 08:24:59 +0800

    練習-停車費

--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Day5_ConsoleApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 計算一天停車費, 一小時5 元, 每天最多收 30元
+			Console.Write("請輸入停車的小時數(1~24):");
+			string input = Console.ReadLine();
+
+			// 在本練習裡,不特別針對輸入值是否正確進行檢查
+			int hours = int.Parse(input);
+
+			int fee = Q1(hours);
+
+			Console.WriteLine($"停車時數={hours}, 停車費用為 {fee}");
+		}
+		static int Q1(int hours)
+		{
+            // todo
+            if (hours >= 6)
+            {
+				return 30;
+            }
+            else
+            {
+				return hours*5;
+            }
+
+
+		}
+	}
 }
